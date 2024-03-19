09f6b8485742ae557f907ddd50e62155c3f382f0
t0rs <torhakn@gmail.com>
2024-03-19 18:53:14 +0100

    Minor changes to zkvm for testing, no longer compatible with /script and /verify

--- sp1/program/src/main.rs
+++ sp1/program/src/main.rs
@@ -3,15 +3,19 @@ sp1_zkvm::entrypoint!(main);
 
 pub fn main() {
     // Predefined credentials
-    let expected_username_hash = 123456789u128; // Example hash of "user123"
-    let expected_password_hash = 987654321u128; // Example hash of "pass123"
+    // let expected_username_hash = 123456789u128; // Example hash of "user123"
+    // let expected_password_hash = 987654321u128; // Example hash of "pass123"
+
+    // Dummy username test
+    let expected_username: String = "red".to_string();
+    let expected_password: String = "bull".to_string();
 
     // Read username and password hashes from input
-    let username_hash = sp1_zkvm::io::read::<u128>();
-    let password_hash = sp1_zkvm::io::read::<u128>();
+    let username_hash = sp1_zkvm::io::read::<String>();
+    let password_hash = sp1_zkvm::io::read::<String>();
 
     // Check if the provided credentials match the expected ones
-    let credentials_match = (username_hash == expected_username_hash) && (password_hash == expected_password_hash);
+    let credentials_match = (username_hash == expected_username) && (password_hash == expected_password);
 
     // Write the result
     sp1_zkvm::io::write(&credentials_match);
