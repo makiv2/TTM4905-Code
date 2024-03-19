bc975397c738bf7b6d336ae507bf0a58220363d7
t0rs <torhakn@gmail.com>
2024-03-19 18:52:17 +0100

    Redesign auxiliary program to take users from the database and run them trough the ELF

--- sp1/auxiliary/src/main.rs
+++ sp1/auxiliary/src/main.rs
@@ -1,5 +1,7 @@
 use reqwest::Error;
 use serde::Deserialize;
+use sp1_core::{SP1Prover, SP1Stdin, SP1Stdout};
+use std::fs;
 
 #[derive(Deserialize, Debug)]
 struct User {
@@ -7,6 +9,8 @@ struct User {
     username: String,
     password: String,
 }
+
+const ELF: &[u8] = include_bytes!("../../program/elf/riscv32im-succinct-zkvm-elf");
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
@@ -19,9 +23,26 @@ async fn main() -> Result<(), Error> {
         .json::<Vec<User>>()
         .await?;
 
-    // Iterate through the fetched users and print them
+    // Iterate through the fetched users and test each one with the prover program
     for user in response {
-        println!("ID: {}, Username: {}, Password: {}", user.id, user.username, user.password);
+        // Generate input for the current user
+        let mut stdin = SP1Stdin::new();
+        let username: String = user.username.clone();
+        let password: String = user.password.clone();
+        stdin.write(&username);
+        stdin.write(&password);
+
+        // Execute the ELF binary with the input
+        let mut stdout = SP1Prover::execute(ELF, stdin).expect("execution failed");
+
+        // Read output from the execution
+        let credentials_match = stdout.read::<bool>();
+
+        if credentials_match {
+            println!("Matching user found:");
+            println!("ID: {}, Username: {}, Password: {}", user.id, user.username, user.password);
+            break;
+        }
     }
 
     Ok(())
