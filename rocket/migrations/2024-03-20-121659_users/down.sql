0be1314afd4e50da799743049084bf57b90de1f8
Matija Popovic <matijapopovic001212@gmail.com>
2024-03-20 13:22:38 +0100

    fix migrations

--- rocket/migrations/2024-03-20-121659_users/down.sql
+++ rocket/migrations/2024-03-20-121659_users/down.sql
@@ -1,9 +1,2 @@
 -- This file should undo anything in `up.sql`
-CREATE TABLE "posts"(
-	"id" INT4 NOT NULL PRIMARY KEY,
-	"title" VARCHAR NOT NULL,
-	"body" TEXT NOT NULL,
-	"published" BOOL NOT NULL
-);
-
 DROP TABLE IF EXISTS "users";
--- rocket/migrations/2024-03-20-121659_users/up.sql
+++ rocket/migrations/2024-03-20-121659_users/up.sql
@@ -1,5 +1,4 @@
 -- Your SQL goes here
-DROP TABLE IF EXISTS "posts";
 CREATE TABLE "users"(
 	"id" INT4 NOT NULL PRIMARY KEY,
 	"username" VARCHAR NOT NULL,
