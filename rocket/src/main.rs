0d280d97ba30a2f73bb874e69714689986f5c05e
Matija Popovic <matijapopovic001212@gmail.com>
2024-04-04 20:00:47 +0200

    init user and proof repository modules

--- rocket/src/main.rs
+++ rocket/src/main.rs
@@ -4,22 +4,18 @@ mod repository;
 mod schema;
 mod services;
 
-
 #[macro_use]
 extern crate rocket;
 
-use rocket::{get, http::Status, serde::json::Json};
-
+use rocket::{get};
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
-
 #[launch]
 fn rocket() -> _ {
-
     // Create any necessary dependencies for UserService
     // let user_repository = repository::UserRepository::new();
     // Create UserService with dependencies and manage it with rocket with .manage() this doesnt work but i dont think its worth fixing
@@ -30,4 +26,3 @@ fn rocket() -> _ {
         .mount("/", routes![api::create_user, api::get_users, api::get_user, api::delete_user,
                             api::generate, api::verify])
 }
-
