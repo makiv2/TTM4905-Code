4f9126fda4241a83513e932abe172bece246510f
Matija Popovic <matijapopovic001212@gmail.com>
2024-04-04 13:02:05 +0200

    add proofs schema

--- rocket/src/main.rs
+++ rocket/src/main.rs
@@ -21,15 +21,13 @@ fn index() -> &'static str {
 fn rocket() -> _ {
 
     // Create any necessary dependencies for UserService
-    let user_repository = repository::UserRepository::new();
-
-    // Create UserService with dependencies
-    let user_service = services::UserService::new(user_repository);
+    // let user_repository = repository::UserRepository::new();
+    // Create UserService with dependencies and manage it with rocket with .manage() this doesnt work but i dont think its worth fixing
+    // let user_service = services::UserService::new(user_repository);
 
     rocket::build()
         .mount("/", routes![index])
         .mount("/", routes![api::create_user, api::get_users, api::get_user, api::delete_user,
                             api::generate, api::verify])
-        .manage(user_service)
 }
 
