6acefcf5e23a84280437516d001c2df45f821a75
Matija Popovic <matijapopovic001212@gmail.com>
2024-04-04 13:05:12 +0200

    apply migrations

--- rocket/src/schema.rs
+++ rocket/src/schema.rs
@@ -1,6 +1,15 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
+    proofs (id) {
+        id -> Int4,
+        proof -> Text,
+        company -> Varchar,
+        message -> Text,
+    }
+}
+
+diesel::table! {
     users (id) {
         id -> Int4,
         username -> Varchar,
@@ -9,11 +18,7 @@ diesel::table! {
     }
 }
 
-diesel::table! {
-    proofs (id) {
-        id -> Int4,
-        proof -> Text,
-        company -> Varchar,
-        message -> Text,
-    }
-}
+diesel::allow_tables_to_appear_in_same_query!(
+    proofs,
+    users,
+);
