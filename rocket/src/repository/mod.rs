da316e03f0cd6cb3c013449f9ad7bded92e914d3
Matija Popovic <matijapopovic001212@gmail.com>
2024-04-05 16:07:12 +0200

    move user repository in user_repo.rs

--- rocket/src/repository/mod.rs
+++ rocket/src/repository/mod.rs
@@ -1,74 +1,16 @@
 mod user_repository;
 mod zk_repository;
 
+
 use std::env;
 use diesel::prelude::*;
 use dotenvy::dotenv;
-use crate::models::{User};
-use crate::schema::users::dsl::*;
+
 
 pub struct UserRepository {
     connection: PgConnection,
 }
 
-
-impl UserRepository {
-    pub fn new() -> Self {
-        let connection = establish_connection_pg();
-        UserRepository { connection }
-    }
-
-    pub async fn create_user(&mut self, user: User) -> User {
-        diesel::insert_into(users)
-            .values(&user)
-            .execute(&mut self.connection)
-            .expect("Error saving new user");
-        user
-    }
-
-    pub async fn get_users(&mut self) -> Result<Vec<User>, &'static str> {
-
-        let results = users
-            .limit(5)
-            .load::<User>(&mut self.connection)
-            .expect("Error loading users");
-
-        return Ok(results);
-    }
-
-    pub async fn get_user(&mut self, user_id: i32) -> Result<User, &'static str> {
-        let result = users.find(user_id).first::<User>(&mut self.connection);
-
-        match result {
-            Ok(user) => Ok(user),
-            Err(_) => Err("User not found"),
-        }
-    }
-
-    pub(crate) async fn update_user(&self, p0: i32, p1: User) {
-        todo!()
-    }
-
-   // pub async fn update_user(&mut self, other_id: i32, updated_user: User) -> Result<User, &'static str> {
-   //     let result = diesel::update(users.find(id))
-   //         .set(&updated_user)
-   //         .get_result(&mut self.connection);
-
-   //     match result {
-   //         Ok(user) => Ok(user),
-   //         Err(_) => Err("Failed to update user"),
-   //     }
-   // }
-
-    pub async fn delete_user(&mut self, user_id: i32) -> Result<(), &'static str> {
-        let result = diesel::delete(users.find(user_id)).execute(&mut self.connection);
-
-        match result {
-            Ok(_) => Ok(()),
-            Err(_) => Err("Failed to delete user"),
-        }
-    }
-}
 
 fn establish_connection_pg() -> PgConnection {
     dotenv().ok();
