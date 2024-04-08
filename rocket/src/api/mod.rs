6e3861fe9d53a97ee4a77c225bb1d100025b67ba
Matija Popovic <matijapopovic001212@gmail.com>
2024-04-08 15:42:40 +0200

    healthcheck

--- rocket/src/api/mod.rs
+++ rocket/src/api/mod.rs
@@ -5,3 +5,10 @@ use rocket::response::{Debug};
 
 // Types
 type Result<T, E = Debug<diesel::result::Error>> = std::result::Result<T, E>;
+
+
+// Health check that just simply returns 200 OK on a get request
+#[get("/health")]
+pub fn health() -> &'static str {
+    "200 OK"
+}
