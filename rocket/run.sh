5e649f9afbc7d1d57963707d42731713d550aa50
Matija Popovic <matijapopovic001212@gmail.com>
2024-04-10 14:48:58 +0200

    sp1 in the rust rocket docker container

--- rocket/run.sh
+++ rocket/run.sh
@@ -1,5 +1,19 @@
 #! /bin/bash
 
+
+# Just in case to make sure the rocket backend does not mess with sp1
+mkdir /sp1
+cd /sp1
+
+# Install sp1
+curl -sSL https://sp1.succinct.xyz | bash
+source /root/.bashrc
+sp1up
+cargo prove --version
+
+# Go back into the rocket backend (app)
+cd /app
+
 diesel migration run
 
 cargo run --release
