0512b2054922e2783af24ce803ab7197a939e740
Matija Popovic <matijapopovic001212@gmail.com>
2024-04-12 23:55:35 +0200

    build on startup

--- rocket/run.sh
+++ rocket/run.sh
@@ -4,11 +4,15 @@
 # Just in case to make sure the rocket backend does not mess with sp1
 cd /sp1
 
+
 # Install sp1
 curl -sSL https://sp1.succinct.xyz | bash
 source /root/.bashrc
 sp1up
 cargo prove --version
+
+cd /sp1/auxiliary
+cargo build --release
 
 # Go back into the rocket backend (app)
 cd /app
