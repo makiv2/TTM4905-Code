ceee6acf80c75ff546e5a98a31b9cf971d8d7b32
Matija Popovic <matijapopovic001212@gmail.com>
2024-04-13 01:38:43 +0200

    ingest_users function

--- backend/run.sh
+++ backend/run.sh
@@ -2,4 +2,5 @@
 
 .venv/bin/python  manage.py collectstatic --noinput
 .venv/bin/python manage.py migrate
+.venv/bin/python manage.py ingest_users
 exec .venv/bin/uwsgi --emperor /etc/uwsgi/sites/backend.ini
