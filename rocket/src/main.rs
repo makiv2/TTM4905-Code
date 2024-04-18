ea442d73352aa341a12674f1f6c8f256aff44328
t0rs <torhakn@gmail.com>
2024-04-18 20:07:15 +0200

    Working on saving logic

--- rocket/src/main.rs
+++ rocket/src/main.rs
@@ -7,10 +7,17 @@ mod services;
 #[macro_use]
 extern crate rocket;
 
+use std::fs;
+use std::io::stdout;
+
 use rocket::{get, post};
-use rocket::serde::json::Json;
+use rocket::serde::json::{self, Json};
 use serde::Deserialize;
+use serde_json::Value;
+use serde_json::from_str;
+use tokio::fs::File;
 use tokio::process::Command;
+use rocket::tokio::io::AsyncWriteExt;
 use rocket_cors::{AllowedOrigins, CorsOptions};
 
 #[derive(Deserialize)]
@@ -18,6 +25,13 @@ struct Credentials {
     company: String, // Dead code, but probably should be used somewhere?
     username: String,
     password: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct GeneratedProof {
+    verified: bool,
+    company: String,
+    message: String,
 }
 
 #[get("/")]
@@ -29,11 +43,13 @@ fn index() -> &'static str {
 async fn check_credentials(credentials: Json<Credentials>) -> String {
     let username = &credentials.username;
     let password = &credentials.password;
+    let company = &credentials.company;
 
     // Run the script executable
-    let output = Command::new("/sp1/auxiliary/target/release/auxiliary")
+    let output = Command::new("../sp1/auxiliary/target/release/auxiliary")
         .arg(username)
         .arg(password)
+        .arg(company)
         .output()
         .await
         .expect("failed to execute script");
@@ -49,7 +65,48 @@ async fn check_credentials(credentials: Json<Credentials>) -> String {
     if output.status.success() {
         // Get the proof content from the script output
         let proof_content = String::from_utf8_lossy(&output.stdout);
-        proof_content.to_string()
+        
+        // Check if the output is equal to "No matching user found in the database."
+        if proof_content.trim() == "No matching user found in the database." {
+            String::from("Failure")
+        } else {
+            // Read json file created
+        let json_file_path = "credentials_check_proof.json";
+        let json_content = fs::read_to_string(json_file_path).expect("Failed to read JSON file");
+
+        // Parse the JSON content
+        let json: Value = serde_json::from_str(&json_content).expect("Failed to parse JSON");
+
+        // Extract the proof from the JSON
+        let proof = json["proof"].as_str().expect("Proof not found in JSON").to_string();
+
+        // Extract the output from the JSON
+        let output_data = json["stdout"]["buffer"]["data"].as_array().expect("Output data not found in JSON");
+        let mut output_string = String::from_utf8(output_data.iter().map(|v| v.as_u64().unwrap() as u8).collect()).expect("Failed to convert output to string");
+        output_string.remove(0).to_string();
+        output_string.trim().to_string();
+        
+        // The output string is malformed json so we won't use it further but it should be possible
+        println!("{:?}", output_string);
+
+        // Parse the output string
+        println!("Do I fail here?");
+        println!("Output String: {}", output_string);
+        // let unescaped_output = output_string.replace("\\\"", "\"");
+        // let output: Value = serde_json::from_str(&unescaped_output).expect("Failed to parse output string");
+
+        // Extract the match, company, and message from the output
+        // let credentials_match = output["match"].as_bool().unwrap_or(false);
+        // let company = output["company"].as_str().unwrap_or("").to_string();
+        // let message = output["message"].as_str().unwrap_or("").to_string();
+        // String::from("Company: ") + &company + &format!(", Match: {}, Message: {}", credentials_match, message)
+
+        //Try uploading json
+        let new_filename = "test_proof.json";
+        json_content.stream_to_file(new_filename).await.expect("Failed to write to file");
+
+        String::from("We did that shit")
+        }
     } else {
         // Return an error response if the execution failed
         String::from("Error: Failed to execute the credentials check")
