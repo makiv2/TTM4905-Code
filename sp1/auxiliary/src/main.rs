ad91bb68ccd8cb86d3cbf811a484a59585383a5b
t0rs <torhakn@gmail.com>
2024-06-12 03:19:26 +0200

    Change auxiliary script to handle PKI format

--- sp1/auxiliary/src/main.rs
+++ sp1/auxiliary/src/main.rs
@@ -1,44 +1,50 @@
-use argon2::{self, Config};
 use reqwest::Error;
 use serde::Deserialize;
-use sha2::{Digest, Sha512};
 use sp1_core::{SP1Prover, SP1Stdin};
 use std::env;
+use std::fs::File;
+use std::io::{self, Read};
 
 #[derive(Deserialize, Debug)]
-struct User {
-    username: String,
-    password: String,
+struct PubKey {
+    key: String,
 }
 
 const ELF: &[u8] = include_bytes!("../../program/elf/riscv32im-succinct-zkvm-elf");
 
+fn read_file_bytes(path: &str) -> Result<Vec<u8>, io::Error> {
+    let mut file = File::open(path)?;
+    let mut content = Vec::new();
+    file.read_to_end(&mut content)?;
+    Ok(content)
+}
+
+// String to bytes function
+fn str_to_bytes(s: &str) -> Vec<u8> {
+    s.as_bytes().to_vec()
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    // Get the command-line arguments
+    // Read the command-line arguments
     let args: Vec<String> = env::args().collect();
-
-    // Check if the required arguments are provided
-    if args.len() < 5 {
-        println!("Usage: {} <expected_username> <expected_password> <company> <message>", args[0]);
-        return Ok(());
+    if args.len() != 4 {
+        eprintln!("Usage: {} <message.txt> <message.txt.sig> <company>", args[0]);
+        std::process::exit(1);
     }
+    let message_path = &args[1];
+    let signature_path = &args[2];
+    let company_name = &args[3];
 
-    // Extract the expected username and password from the command-line arguments
-    let expected_username: String = args[1].clone();
-    let expected_password: String = args[2].clone();
-    let company: String = args[3].clone();
-    let message: String = args[4].clone();
+    // Read the message from file
+    let message: Vec<u8> = read_file_bytes(message_path).expect("Error reading message file");
+    // Define message as a string
+    let message_str = String::from_utf8(message.clone()).expect("Invalid UTF-8 message");
+    // Print message
+    println!("Message: {:?}", message_str);
 
-    // Hash the expected username using SHA-512
-    let mut hasher = Sha512::new();
-    hasher.update(expected_username.as_bytes());
-    let expected_username_hash: String = format!("{:x}", hasher.finalize());
-
-    // Hash the expected password using Argon2 with a salt
-    let config: Config<'_> = Config::default();
-    let salt: &[u8; 16] = b"some_random_salt";
-    let expected_password_hash: String = argon2::hash_encoded(expected_password.as_bytes(), salt, &config).unwrap();
+    // Read the signature from file
+    let signature_bytes: Vec<u8> = read_file_bytes(signature_path).expect("Error reading signature file");
 
     // URL of the Django API endpoint for fetching users
     // Locally let url = "http://localhost:8000/api/users/";
@@ -47,41 +53,36 @@ async fn main() -> Result<(), Error> {
     let url: String;
 
     if let Ok(django_host) = env::var("DJANGO_HOST") {
-        url = format!("{}/api/users/", django_host);
+        url = format!("{}/api/pubkeys/", django_host);
     } else {
         println!("DJANGO_HOST environment variable is not set.");
         return Ok(());
     }
 
     // Send a GET request to the API endpoint
-    let response: Vec<User> = reqwest::get(&url)
+    let response: Vec<PubKey> = reqwest::get(&url)
         .await?
-        .json::<Vec<User>>()
+        .json::<Vec<PubKey>>()
         .await?;
 
-    // Dummy company name
-    let company_name: String = company;
     // Flag to track if a matching user is found
     let mut user_found: bool = false;
 
     // Iterate through the fetched users and test each one with the prover program
-    for user in response {
-        // Hash the test username using SHA-512
-        let mut hasher = Sha512::new();
-        hasher.update(user.username.as_bytes());
-        let test_username_hash: String = format!("{:x}", hasher.finalize());
+    for pubkey in response {
+        // Make the pubkey into the correct type
+        let pubkey = pubkey.key;
+        let pubkey_bytes = str_to_bytes(&pubkey);
+        // Print public key from pem format
+        println!("Public key: {:?}", String::from_utf8(pubkey_bytes.clone()).expect("Invalid UTF-8 public key"));
 
-        // Hash the test password using Argon2 with the same salt
-        let test_password_hash: String = argon2::hash_encoded(user.password.as_bytes(), salt, &config).unwrap();
-
-        // Generate input for the current user
+        // Generate input for the current pubkey
         let mut stdin: SP1Stdin = SP1Stdin::new();
-        stdin.write(&expected_username_hash);
-        stdin.write(&expected_password_hash);
-        stdin.write(&test_username_hash);
-        stdin.write(&test_password_hash);
-        stdin.write(&company_name);
-        stdin.write(&message);
+        stdin.write(&pubkey_bytes.clone());
+        stdin.write(&message.clone());
+        stdin.write(&signature_bytes.clone());
+        stdin.write(&company_name.clone());
+
 
         // Execute the ELF binary with the input
         let mut stdout: sp1_core::SP1Stdout = SP1Prover::execute(ELF, stdin).expect("execution failed");
@@ -94,28 +95,25 @@ async fn main() -> Result<(), Error> {
 
         // Check the value of the "match" field
         if output["match"].as_bool().unwrap_or(false) {
-            println!("Matching user found:");
-            println!("Username: {}, Password: {}", user.username, user.password);
+            println!("Signature was valid:");
             println!("Company: {}", output["company"].as_str().unwrap_or(""));
-            println!("Message: {}", &message);
+            println!("Message: {}", &message_str);
             user_found = true;
 
             // If user is found, generate the proof, start by creating new stdin
-            let mut new_stdin = SP1Stdin::new(); // Create a new instance of SP1Stdi
+            let mut new_stdin = SP1Stdin::new(); // Create a new instance of SP1Stdin
 
             // Populate it
-            new_stdin.write(&expected_username_hash);
-            new_stdin.write(&expected_password_hash);
-            new_stdin.write(&test_username_hash);
-            new_stdin.write(&test_password_hash);
-            new_stdin.write(&company_name);
-            new_stdin.write(&message);
+            new_stdin.write(&pubkey_bytes.clone());
+            new_stdin.write(&message.clone());
+            new_stdin.write(&signature_bytes.clone());
+            new_stdin.write(&company_name.clone());
 
             let proof = SP1Prover::prove_only_output(ELF, new_stdin).expect("proving failed"); // Use the new instance of SP1Stdin
             let proof_filename = format!("credentials_check_proof.json");
             proof.save(&proof_filename).expect("saving proof failed");
 
-            // Exit program after proof is generated
+            // // Exit program after proof is generated
             break;
         }
     }
